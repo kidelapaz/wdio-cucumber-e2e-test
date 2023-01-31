87715f83cb2fff5d4f0fcc423c3326a5cbbe8f87
kidelapaz <kidelapaz@deloitte.com>
2023-01-31 21:57:24 +0800

    add input handling

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -27,3 +27,35 @@ Then(/^URL should match (.*)$/, async function(expectedURL){
     let url = await browser.getUrl()
     chai.expect(url).to.equal(expectedURL)
 })
+/**
+ * Web Interactions
+ */
+Given(/^A web page is opened$/, async function(){
+    await browser.url("/inputs")
+    await browser.setTimeout({implicit: 15000, pageLoad: 10000})
+    // await browser.maximizeWindow()
+})
+
+When(/^Perform web Interactions$/, async function(){
+    /**
+     * 1. Input box
+     * Actions:
+     * 1. Type into input box
+     * 2. Clear the field and type or just add value
+     * 3. Click and type
+     * 4. Slow typing
+     */
+    let num = 12345
+    let strNum = num.toString()
+    let ele = await $(`[type=number]`)
+    await ele.click()
+    //await ele.setValue(strNum)
+
+    for(let i = 0; i< strNum.length; i++) {
+        let charStr = strNum.charAt(i)
+        await browser.pause(1000)
+        await browser.keys(charStr)
+    }
+
+    await browser.debug()
+})
